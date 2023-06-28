91ce5f3c141a44f31406c510bed27f5eaedbaf08
Novo <120156959+Novo1999@users.noreply.github.com>
2023-06-28 17:13:43 +0600

    Added new welcome feature

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -125,6 +125,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+function newFeature() {
+  console.log('Welcome to the app');
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -133,5 +137,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
